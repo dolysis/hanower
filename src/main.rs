d5b745e7f28b48a323eb6afd04eb6e9b217bd46c
emucc <github@elrimer.com>
2020-11-04 13:45:30 +0000

    started first function

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,33 @@ use clap::Clap;
 #[derive(Clap, Debug)]
 #[clap(version = "3.0.0-beta.2")]
 struct Options {
+    #[clap(short, long, default_value = "1.0")]
     start: usize,
+
+    #[clap(short, long, default_value = "2.0")]
     end: usize,
+
+    #[clap(short, long, default_value = "2")]
     number: usize,
 }
 
-fn main() {
-    let options = Options::parse();
+fn exponential_progression(a: f64, b: f64, c: usize) -> Vec<f64> {
+    if c == 2 {
+        println!("{:?}", vec![a, b]); // remove
+        return vec![a, b];
+    } else if c > 2 {
+        let steps = 1.0;
+        println!("{:?}", vec![a, steps, b]); // remove
+        return vec![a, steps, b];
+    }
 
-    println!("{:?}", options);
+    let placeholder_return = vec![1.0];
+    return placeholder_return;
+}
+
+fn main() {
+    // let options = Options::parse();
+    // println!("{:?}", options);
+
+    exponential_progression(1.0, 2.0, 3);
 }
