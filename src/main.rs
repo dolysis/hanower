f2024e42d03b4081cc0ca92e3e322fc35b5d610b
emucc <github@elrimer.com>
2020-11-10 15:14:31 +0000

    started bucket function

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,17 @@ struct Opts {
     #[structopt(short, long)]
     debug: bool,
 
-    #[structopt(short, long, default_value = "10.0")]
+    #[structopt(default_value = "10.0")]
     start: f64,
 
-    #[structopt(short, long, default_value = "100.0")]
+    #[structopt(default_value = "100.0")]
     end: f64,
 
-    #[structopt(short, long, default_value = "2")]
+    #[structopt(default_value = "2")]
     number: usize,
+
+    #[structopt(short, long)]
+    file: String,
 }
 
 fn find_fenceposts(mut start: f64, mut end: f64, gaps: usize) -> Vec<f64> {
@@ -42,15 +45,28 @@ fn find_fenceposts(mut start: f64, mut end: f64, gaps: usize) -> Vec<f64> {
             increment_value = increment_value + increment;
             return_vec.push(increment_value);
         }
-    }
 
-    let placeholder_return = vec![1.0];
-    return placeholder_return;
+        return return_vec;
+    } else {
+        println!("Gaps too small.");
+        return vec![start, end];
+    }
+}
+
+// uses find_fenceposts() vec to identify buckets
+// and then pulls the index number & row data from the max index
+// in those buckets
+//      placeholder return type
+fn max_bucket_value(fence_vec: Vec<f64>) -> f64 {
+    let min = fence_vec[0];
+    let max = fence_vec[Opts::from_args().number];
+
+    return 4.20; //placeholder return value
 }
 
 fn main() {
     let opt = Opts::from_args();
-    println!("{:#?}", opt);
+    // println!("{:#?}", opt);
 
     find_fenceposts(opt.start, opt.end, opt.number);
 }
