82e4ebdddff4d1aa8eb1ed2de0549d4ba49cba86
Bazaah <github@luxolus.com>
2021-01-21 17:27:07 +0000

    move start,end to unnamed postional args

--- cli/src/cli/subcommand/interval/mod.rs
+++ cli/src/cli/subcommand/interval/mod.rs
@@ -6,19 +6,19 @@
  */
 
 use super::*;
+use structopt::clap::AppSettings::AllowLeadingHyphen;
 
 #[derive(Debug, StructOpt)]
+#[structopt(setting = AllowLeadingHyphen)]
 pub struct SubComInterval {
-    /// To include negative ARGs values, put -- before the list of values{n}
-    /// e.g. hanoi interval --count 8 -- -25 -4{n}
-    #[structopt(default_value = "0")]
-    start: i64,
-
-    #[structopt(default_value = "256")]
-    end: i64,
-
     #[structopt(long, default_value = "2")]
     count: u64,
+
+    #[structopt(allow_hyphen_values = true)]
+    low: i64,
+
+    #[structopt(allow_hyphen_values = true)]
+    high: i64,
 }
 
 impl Runner for SubComInterval {
@@ -29,7 +29,10 @@ impl Runner for SubComInterval {
         dst: &mut dyn std::io::Write,
         _config: Option<Self::Config>,
     ) -> Result<(), color_eyre::eyre::Report> {
-        let interval = core::Interval::new(self.start as f64, self.end as f64, self.count)?;
+        let low = self.low as f64;
+        let high = self.high as f64;
+
+        let interval = core::Interval::new(low, high, self.count)?;
         let output: Vec<f64> = interval.intervals().collect();
 
         for &number in &output {
